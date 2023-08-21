0b2e8ffc2ffbdbf45b09eefcaaa9c285cb30efa9
NicolasChirazawa <nicolaschfernandes@gmail.com>
2023-08-21 13:16:40 -0300

    Começo dos estudos

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+
+      <h1> Olá React</h1>
+      <h2>Alterando o JSX</h2>
+      <p> Meu Primeiro App</p>
+
+    </div>
+
+  );
+}
+
+export default App;
